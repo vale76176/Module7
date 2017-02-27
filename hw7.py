bbad1da5c49ae53c0dc02c78ed065a709ff26c70
Dulce Carrillo <Dulce Carrillo>
2017-02-26 20:33:00 -0800

    adding homework 7

--- hw7.py
+++ hw7.py
@@ -1,5 +1,6 @@
 from sortedcontainers import SortedDict
 
+
 def print_menu():
     print('1. Print Users')
     print('2. Add a User')
@@ -7,6 +8,7 @@ def print_menu():
     print('4. Lookup a Phone Number')
     print('5. Quit')
     print()
+
 
 # Create dictionary with key = Names, value = user_name
 usernames = SortedDict()
@@ -19,43 +21,43 @@ usernames['Renato'] = 'songDude'
 # setup counter to store menu choice
 menu_choice = 0
 
-#display your menu
+# display your menu
 print_menu()
 
 # as long as the menu choice isn't "quit" get user options
 while menu_choice != 5:
     # get menu choice from user
     menu_choice = int(input("Type in a number (1-5): "))
-    
+
     # view current entries
     if menu_choice == 1:
         print("Current Users:")
-        for x,y in usernames.items():
-            print("Name: {} \tUser Name: {} \n".format(x,y))
-            
+        for x, y in usernames.items():
+            print("Name: {} \tUser Name: {} \n".format(x, y))
+
     # add an entry
     elif menu_choice == 2:
         print("Add User")
         name = input("Name: ")
         username = input("User Name: ")
         usernames[name] = username
-        
+
     # remove an entry
     elif menu_choice == 3:
         print("Remove User")
         name = input("Name: ")
         if name in usernames:
-            pass # delete that entry
+            pass  # delete that entry
 
-    # view user name      
+    # view user name
     elif menu_choice == 4:
         print("Lookup User")
         name = input("Name: ")
         if name in usernames:
-            pass # print the username
+            pass  # print the username
         else:
-            pass # print username not found
-    
+            pass  # print username not found
+
     # is user enters something strange, show them the menu
     elif menu_choice != 5:
         print_menu()
